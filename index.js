a7d2dde0c91f4eecd08a52c72c62d40d0344f46a
Emad0007 <BASEMRF007@GMAIL.COM>
2022-06-22 12:55:21 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict'
+
+/*1
+
+const  fruits = ["mango","lemon","blueberry"]
+
+console.log([fruits])
+console.log(fruits)
+
+*/
+
+
+/*2
+
+const ingredient = ["eggs","milk","butter"]
+
+ console.log(ingredient[1])
+
+console.log(ingredient.indexOf("butter"))
+*/
+
+
+/*3
+const object = ["pen","book","lamp"]
+object.unshift("chair")
+
+console.log(object)
+
+object.pop("lamp")
+
+console.log(object)
+
+
+object.push("laptop")
+
+console.log(object)
+
+
+object.shift("chair")
+
+console.log(object)
+
+*/
+
+/*4
+
+const numbers = ["3","10","8","12","6"]
+
+numbers.reverse(numbers)
+
+console.log(numbers)
+
+*/
+
+/*5
+
+let total = 0
+const limit = 10
+
+for (let i = 0 ; i <= 10  ; i++ ) {
+    console.log(i)
+    total = total + i
+   
+}
+
+console.log(total)
+
+*/
+
+/*6
+
+const sentence = "Hallo Konexio!"
+
+for (let i = sentence.length -1 ;   i >= 0  ; i--) {
+    console.log(sentence.charAt(i))
+
+}
+
+*/
+
+
+/*7 */
