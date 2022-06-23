f628cdfa4b38fe9422dce0257f8c651a0f5b5ff8
Emad0007 <BASEMRF007@GMAIL.COM>
2022-06-23 06:17:58 +0100

    add question to solve

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 /*1
 
+## 01 - Fruits
+
+- Créez une const `fruits` qui contiendra un tableau avec les valeurs `"mango"`, `"lemon"` et `"blueberry"`
+- Affichez `fruits` dans la console
+- Bonus: essayez `console.table(fruits)`
+
+
+
 const  fruits = ["mango","lemon","blueberry"]
 
 console.log([fruits])
@@ -11,6 +19,12 @@ console.log(fruits)
 
 
 /*2
+## 02 - Access
+
+- Créez une const `ingredients` qui contiendra un tableau avec les valeurs `"eggs"`, `"milk"` et `"butter"`
+- Faites apparaître `"milk"` dans la console
+- Faites apparaître l'index de `"butter"` dans la console
+
 
 const ingredient = ["eggs","milk","butter"]
 
@@ -21,6 +35,14 @@ console.log(ingredient.indexOf("butter"))
 
 
 /*3
+## 03 - Add and Remove
+
+- Créez une const `objects` qui contiendra un tableau avec les valeurs `"pen"`, `"book"` et `"lamp"`
+- Ajoutez `"chair"` au début du tableau puis affichez le résultat
+- Enlevez `"lamp"` puis affichez le résultat
+- Ajoutez `"laptop"` à la fin du tableau puis affichez le résultat
+- Enlevez `"chair"` puis affichez le résultat
+
 const object = ["pen","book","lamp"]
 object.unshift("chair")
 
@@ -43,6 +65,10 @@ console.log(object)
 */
 
 /*4
+## 4 - Order
+
+- Créez une const `numbers` qui contiendra un tableau avec les valeurs `4`, `10`, `8`, `12` et `6`
+- Inversez l'ordre des éléments de `numbers` puis affichez le résultat
 
 const numbers = ["3","10","8","12","6"]
 
@@ -68,6 +94,10 @@ console.log(total)
 */
 
 /*6
+## 06 - Reverse
+
+- Créez une const `sentence` ayant pour valeur "Hello Konexio !"
+- A l'aide d'une boucle for et sans utilisez la fonction .reverse(), faites en sorte d'afficher l'inverse de votre variable `sentence` ("! oixenoK olleH")
 
 const sentence = "Hallo Konexio!"
 
@@ -79,10 +109,20 @@ for (let i = sentence.length -1 ;   i >= 0  ; i--) {
 */
 
 
-/*7 */
+/*7 
 
+ 07 - Fizzbuzz
 
+- Créez une boucle qui part de 0 et s'arrête à 100
+- À chaque itération :
+    - Si `i` est un multiple de 3 ⇒ affichez "fizz"
+    - Si `i` est un multiple de 5 ⇒ affichez "buzz"
+    - Si `i` est un multiple de 3 et 5 ⇒ affichez "fizzbuzz"
+    - Si `i` est un multilple de 7 ⇒ ne l'affichez pas
+    - Sinon, affichez la valeur de i
 
+*Indice: utilisez modulo*
+*/
 // let end = 100
 
 // for ( let i = 0 ; i <=100; i++) {
@@ -112,7 +152,10 @@ for (let i = sentence.length -1 ;   i >= 0  ; i--) {
     
 
 
-/*8*/
+/*8
+ 08 - While
+
+- Reproduisez l'exercice 5 avec une boucle while
 
 // let total = 0
 // let limit = 10
@@ -127,7 +170,10 @@ for (let i = sentence.length -1 ;   i >= 0  ; i--) {
 // }
 
 
+## 09 - Sans boucle
 
+- Créez un tableau avec le nom de chacun de vos camarades
+- Affichez aléatoirement le nom de la personne tiré au sort
 
 
 //  const friends = ["Danh","walid1","walid2","Edaly"]
@@ -141,7 +187,13 @@ for (let i = sentence.length -1 ;   i >= 0  ; i--) {
 //  console.log(friends[random])
 
 
-/*10*/
+/*10
+## 10 - Random and max
+
+- Créez un tableau vide
+    - Avec une boucle for, ajoutez 20 entiers aléatoires entre 0 et 100
+- Affichez ce tableau
+- Avec une deuxième boucle for, trouvez l'entier le plus grand de votre tableau (pas le droit d'utiliser Math.max
 
 // const table = []
 
@@ -160,4 +212,4 @@ for (let i = sentence.length -1 ;   i >= 0  ; i--) {
 
 // }
 
-// console.log(max)
+// console.log(max) */
